1e22642c1fb6801bccfe47b4589858657de479be
electricalvlab <121746390+electricalvlab@users.noreply.github.com>
2023-01-29 15:47:49 +0530

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,11 @@
+$(document).on("click","#cust_btn",function(){
+  
+   $("#myModal").modal("toggle");
+   
+ })
+
+ $(document).on("click","#cust_btn1",function(){
+  
+   $("#myModal1").modal("toggle");
+   
+ })
---
+++ js/script.js
@@ -0,0 +1,51 @@
+function scrollWaypointInit( items, trigger ) {
+  items.each( function() {
+    var element = $(this),
+        osAnimationClass = element.data("animation"),
+        osAnimationDelay = element.attr('data-animation-delay');
+ 
+    element.css({
+        '-webkit-animation-delay':  osAnimationDelay,
+        '-moz-animation-delay':     osAnimationDelay,
+        'animation-delay':          osAnimationDelay
+    });
+ 
+    var trigger = ( trigger ) ? trigger : element;
+ 
+    trigger.waypoint(function() {
+        element.addClass('animated').addClass(osAnimationClass);
+    },{
+        triggerOnce: true,
+        offset: '80%'
+    });
+  });
+}
+
+//Call the init
+
+$(document).ready(function(){
+
+scrollWaypointInit( $('.animateMe') );
+
+});
+
+
+
+
+//google sign in
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+// //reCAPTCHA
+// var onloadCallback = function() {
+//   alert("grecaptcha is ready!");
+// };
+
+
+
+/////////////////////// CLOCK //////////////////////
